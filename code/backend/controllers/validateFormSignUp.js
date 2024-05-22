d79136dfc907b4e0ee5e044be767db0ade51b490
Calu12 <coemgen.genevey@gmail.com>
2024-05-22 11:26:36 +0200

    correction bug

--- code/backend/controllers/validateFormSignUp.js
+++ code/backend/controllers/validateFormSignUp.js
@@ -2,24 +2,24 @@ const Yup = require('yup');
 
 
 const formSchemaSignUp = Yup.object().shape({
-    username: Yup.string().required("Nom d'utilisateur requis").min(6, "Le nom d'utilisateur doit contenir au moins 6 caractères").max(30, "Le nom d'utilisateur doit contenir au plus 30 caractères"),  
+    username: Yup.string().required("Nom d'utilisateur requis").min(4, "Le nom d'utilisateur doit contenir au moins 4 caractères").max(30, "Le nom d'utilisateur doit contenir au plus 30 caractères"),  
     email: Yup.string().email("Email non valide").required("Email requis"),
     password: Yup.string().required("Mot de passe requis").min(6, "Le mot de passe doit contenir au moins 6 caractères"),
-    cohorte: Yup.string().required("Cohorte requise").min(1, "La cohorte doit contenir au moins 1 caractères").max(30, "La cohorte doit contenir au plus 30 caractères"),
+    cohorte: Yup.string().required("Cohorte requise").min(2, "La cohorte doit contenir au moins 1 caractères").max(30, "La cohorte doit contenir au plus 30 caractères"),
 });
 
-const validateFormSignUp = (req, res) => {
+const validateFormSignUp = async (req, res) => {
     const formeData = req.body;
-    formSchemaSignUp
+    try{
+    await formSchemaSignUp
         .validate(formeData)
-        .catch(err => {
-            res.status(422).send();
+        console.log("Formulaire validé");
+    }
+        catch(err) {
             console.log(err.errors);
-    }).then(valid => {  
-        if (valid) {
-            console.log("Formulaire validé");
-        }
-    });
+            return res.status(422).send({ error: err.errors });
+    }
+    
 };
 
 module.exports = validateFormSignUp;
--- code/backend/routers/authRouter.js
+++ code/backend/routers/authRouter.js
@@ -9,11 +9,11 @@ const validateFormSignUp = require('../controllers/validateFormSignUp');
 router
     .route("/login")
     .get(async (req, res) => {
-        if(req.session.user && req.session.user.username){
+        if (req.session.user && req.session.user.username) {
             console.log(req.session.user.username);
-            res.json({ LoggedIn : true, username: req.session.user.username, statut: req.session.user.statut, cohorte: req.session.user.cohorte});
-        }else{
-            res.json({ LoggedIn : false});
+            res.json({ LoggedIn: true, username: req.session.user.username, statut: req.session.user.statut, cohorte: req.session.user.cohorte });
+        } else {
+            res.json({ LoggedIn: false });
         }
     })
     .post(async (req, res) => {
@@ -23,63 +23,72 @@ router
             'SELECT id, username, passhash, statut, cohorte FROM users u WHERE u.email = $1',
             [req.body.email]
         );
-        if(potentialLogin.rowCount > 0){
+        if (potentialLogin.rowCount > 0) {
             const isSamePass = await bcrypt.compare(
-                req.body.password, 
+                req.body.password,
                 potentialLogin.rows[0].passhash
             );
-            if(isSamePass){
+            if (isSamePass) {
                 req.session.user = {
-                    username:potentialLogin.rows[0].username,
-                    id:potentialLogin.rows[0].id,
-                    statut:potentialLogin.rows[0].statut,
-                    cohorte:potentialLogin.rows[0].cohorte
+                    username: potentialLogin.rows[0].username,
+                    id: potentialLogin.rows[0].id,
+                    statut: potentialLogin.rows[0].statut,
+                    cohorte: potentialLogin.rows[0].cohorte
                 }
-                res.json({ LoggedIn : true, username: potentialLogin.rows[0].username, statut: potentialLogin.rows[0].statut, cohorte: potentialLogin.rows[0].cohorte});
-            }else{
-                res.json({ LoggedIn : false, status : "Email ou mot de passe incorrect !"});
+                res.json({ LoggedIn: true, username: potentialLogin.rows[0].username, statut: potentialLogin.rows[0].statut, cohorte: potentialLogin.rows[0].cohorte });
+            } else {
+                res.json({ LoggedIn: false, status: "Email ou mot de passe incorrect !" });
             }
-        }else{
-            res.json({ LoggedIn : false, status : "Email ou mot de passe incorrect !"});
+        } else {
+            res.json({ LoggedIn: false, status: "Email ou mot de passe incorrect !" });
         }
     });
 
 router.post('/register', async (req, res) => {
-    validateFormSignUp(req, res);
+    const validation = validateFormSignUp(req, res);
+    try{
 
-    const existingUser = await pool.query(
-        'SELECT username FROM users WHERE username = $1', 
-        [req.body.username]
-    );
-
-    if(existingUser.rowCount == 0){
-        const existingEmail = await pool.query(
-            'SELECT username FROM users WHERE email = $1', 
-            [req.body.email]
+        const existingUser = await pool.query(
+            'SELECT username FROM users WHERE username = $1',
+            [req.body.username]
         );
-        if(existingEmail.rowCount == 0){
-            const hashedPass = await bcrypt.hash(req.body.password, 10);
 
-            const newUserQuery = await pool.query(
-                'Insert INTO users (username, passhash, email, statut, cohorte) VALUES ($1, $2, $3, $4, $5) RETURNING id, username, statut, cohorte',
-                [req.body.username, hashedPass, req.body.email, 'étudiant', req.body.cohorte]
+        if (existingUser.rowCount == 0) {
+            const existingEmail = await pool.query(
+                'SELECT username FROM users WHERE email = $1',
+                [req.body.email]
             );
-            req.session.user = {
-                username:newUserQuery.rows[0].username,
-                id:newUserQuery.rows[0].id,
-                statut:newUserQuery.rows[0].statut,
-                cohorte:newUserQuery.rows[0].cohorte
+            if (existingEmail.rowCount == 0) {
+                const hashedPass = await bcrypt.hash(req.body.password, 10);
+
+                const newUserQuery = await pool.query(
+                    'Insert INTO users (username, passhash, email, statut, cohorte) VALUES ($1, $2, $3, $4, $5) RETURNING id, username, statut, cohorte',
+                    [req.body.username, hashedPass, req.body.email, 'étudiant', req.body.cohorte]
+                );
+                req.session.user = {
+                    username: newUserQuery.rows[0].username,
+                    id: newUserQuery.rows[0].id,
+                    statut: newUserQuery.rows[0].statut,
+                    cohorte: newUserQuery.rows[0].cohorte
+                }
+                res.json({ LoggedIn: true, username: req.body.username, statut: newUserQuery.rows[0].statut, cohorte: newUserQuery.rows[0].cohorte });
             }
-            res.json({ LoggedIn : true, username: req.body.username, statut: newUserQuery.rows[0].statut, cohorte: newUserQuery.rows[0].cohorte});
+            else {
+                res.json({ LoggedIn: false, status: "Email déjà utilisé" });
+            }
+
         }
         else {
-            res.json({ LoggedIn : false, status : "Email déjà utilisé"});
+            res.json({ LoggedIn: false, status: "Nom d'utilisateur déjà utilisé" });
         }
-        
+    } else {
+        res.json({ LoggedIn: false, status: validation.status });
     }
-    else {
-        res.json({ LoggedIn : false, status : "Nom d'utilisateur déjà utilisé"});
-    }
+}catch(err){
+    console.log(err);
+    res.json({ LoggedIn: false, status: err.errors });
+
+}
 });
 
 module.exports = router;
