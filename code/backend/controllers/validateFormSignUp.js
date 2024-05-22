460a9646fe251c622d5bc3ac5556df002151911d
miges <miguelangelespinal@gmail.com>
2024-05-22 11:25:01 +0200

    idem

--- code/backend/controllers/validateFormSignUp.js
+++ code/backend/controllers/validateFormSignUp.js
@@ -2,7 +2,7 @@ const Yup = require('yup');
 
 
 const formSchemaSignUp = Yup.object().shape({
-    username: Yup.string().required("Nom d'utilisateur requis").min(4, "Le nom d'utilisateur doit contenir au moins 6 caractères").max(30, "Le nom d'utilisateur doit contenir au plus 30 caractères"),  
+    username: Yup.string().required("Nom d'utilisateur requis").min(4, "Le nom d'utilisateur doit contenir au moins 4 caractères").max(30, "Le nom d'utilisateur doit contenir au plus 30 caractères"),  
     email: Yup.string().email("Email non valide").required("Email requis"),
     password: Yup.string().required("Mot de passe requis").min(6, "Le mot de passe doit contenir au moins 6 caractères"),
     cohorte: Yup.string().required("Cohorte requise").min(1, "La cohorte doit contenir au moins 1 caractères").max(30, "La cohorte doit contenir au plus 30 caractères"),
