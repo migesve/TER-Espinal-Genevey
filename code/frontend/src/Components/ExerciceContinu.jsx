e9a81b9e4dafc03130246dbfc279f13538b33f6b
miges <miguelangelespinal@gmail.com>
2024-05-28 15:55:33 +0200

    using rc-knob for rotation and localStorage for user login

--- code/frontend/src/Components/ExerciceContinu.jsx
+++ code/frontend/src/Components/ExerciceContinu.jsx
@@ -1,35 +1,39 @@
 import React, { useState, useRef } from 'react';
-import Draggable from 'react-draggable';
+import { Knob, Value, Pointer } from 'rc-knob';
 
 export const ExerciceContinu = () => {
   const [rotation, setRotation] = useState(0);
 
-  const handleDragRotate = (e, data) => {
-    const newRotation = rotation + data.deltaX;
+  const handleDragRotate = (data) => {
+    const newRotation = data;
     setRotation(newRotation);
   };
-
-  const nodeRef = useRef(null);
-
   return (
     <div className="flex flex-col items-center p-4 min-h-screen">
-      <Draggable nodeRef={nodeRef} onDrag={handleDragRotate}>
-        <p ref={nodeRef}>ROTAR</p>
-      </Draggable>
+      <Knob
+        size={200}
+        min={0}
+        max={360}
+        onChange={(value) => handleDragRotate(value)}
+      >
+        <Pointer width={3} radius={80} type="circle" color="#FC5A96" />
+      </Knob>
       <div className="relative flex items-center w-[384px] h-[288px]">
         <img
           src="images/bassin.png"
           alt="Bassin"
           className="absolute flex items-center inset-0 object-cover w-full h-fit z-20 pointer-events-none"
         />
-        <div className="absolute flex items-center inset-0 transform-gpu z-10">
+        <div
+          className="absolute flex items-center inset-0 transform-gpu z-10"
+          style={{
+            transform: `rotate(${rotation}deg)`,
+          }}
+        >
           <img
             src="images/tete.png"
             alt="Tete"
             className="object-cover w-full h-fit"
-            style={{
-              transform: `rotate(${rotation}deg)`,
-            }}
           />
           <div className="absolute flex items-center inset-0 transform-gpu z-0">
             <img
