9aa40585af9425f23267b2d5186cda22cc2e3b9f
miges <miguelangelespinal@gmail.com>
2024-05-29 15:57:14 +0200

    un peu a la main la rotation

--- code/frontend/src/Components/ExerciceContinu.jsx
+++ code/frontend/src/Components/ExerciceContinu.jsx
@@ -7,35 +7,9 @@ export const ExerciceContinu = ({display}) => {
   return (
     <>
       <div
-        className={`${display} flex-col items-center p-4 min-h-screen w-[384px] h-[288px]`}
+        className={`${display} flex-col items-center p-4`}
       >
-        <div className="flex relative justify-center items-center w-full h-full">
-          <img
-            src="images/bassin.png"
-            alt="Bassin"
-            className="absolute inset-0 object-cover w-full h-full z-30 pointer-events-none"
-          />
-          <div
-            className="absolute inset-0 transform-gpu z-20 pointer-events-none"
-            style={{
-              transform: `rotate(${rotation}deg)`,
-            }}
-          >
-            <img
-              src="images/tete.png"
-              alt="Tete"
-              className="object-cover w-full h-full"
-            />
-            <div className="absolute inset-0 transform-gpu z-30 pointer-events-none">
-              <img
-                src="images/fontanelles.png"
-                alt="Fontanelles"
-                className="object-cover w-full h-full"
-              />
-            </div>
-          </div>
-        </div>
-        <RotationKnob rotation={rotation} setRotation={setRotation} />
+        <RotationKnob />
       </div>
     </>
   );
