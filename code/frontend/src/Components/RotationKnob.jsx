62d046be049823875f85ffdb3c2895c8f41525e8
miges <miguelangelespinal@gmail.com>
2024-05-29 11:59:08 +0200

    Login localStorage et exo continu presque

---
+++ code/frontend/src/Components/RotationKnob.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Knob, Pointer } from 'rc-knob';
+
+const RotationKnob = ({ rotation, setRotation }) => {
+  const handleDragRotate = (value) => {
+    // Ensure the state update happens outside the render phase
+    requestAnimationFrame(() => {
+      setRotation(value);
+    });
+  };
+
+  return (
+    <Knob size={200} min={0} max={360} value={rotation} onChange={handleDragRotate}>
+      <Pointer width={3} radius={80} type="circle" color="#FC5A96" />
+    </Knob>
+  );
+};
+
+export default RotationKnob;
